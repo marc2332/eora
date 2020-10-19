044b3d928de82138246630a1cae3bda68d1ef35a
Marc Espín <mespinsanz@gmail.com>
2020-10-19 12:27:34 +0200

    :sparkles: Implemented autocompletion

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ let pwd = process.cwd()
 import chalk from 'chalk'
 import chalkRainbow from 'chalk-rainbow'
 import path from 'path'
-import fs from 'fs'
+import fs from 'fs-extra'
 import cp from'child_process'
 import * as theme from './theme'
+import keypress from 'keypress'
 
 let leave = false
 let shellFocused = true
@@ -21,7 +22,14 @@ const parseComand = (args) => {
 	}
 }
 
-const out = text => process.stdout.write(text);
+const out = text => {
+	console.log('')
+	process.stdout.write(text)
+}
+
+const promptPrint = text => {
+	process.stdout.write(text)
+}
 
 const cleanArgs = args => args.map(a => a.replace(/(\r\n|\n|\r)/gm,''))
 
@@ -32,7 +40,7 @@ const executeCommand = async ({ cmd, args}) => {
 			case 'ls':
 				fs.readdir(pwd,(err, list) => {
 					if(err) out(err)
-					out('\n'+list.join('\n')+'\n')
+					out(list.join('\n'))
 					res()
 				})
 				break;
@@ -43,6 +51,10 @@ const executeCommand = async ({ cmd, args}) => {
 				break;
 			case 'clear':
 				console.clear()
+				res()
+				break;
+			case 'echo':
+				out(args.join(' '))
 				res()
 				break;
 			case 'pwd':
@@ -117,8 +129,6 @@ const executeCommand = async ({ cmd, args}) => {
 					ps.stdin.write(input.toString())
 				}
 				
-				process.stdin.on('data', writingListener)
-				
 				ps.stdout.on('data', (data) => {
 					out(data)
 				})
@@ -128,21 +138,147 @@ const executeCommand = async ({ cmd, args}) => {
 	})
 }
 
-process.stdin.on('data', async input  => {
-	if(!shellFocused) return
-	const command = parseComand(input.toString().split(' '))
-	await executeCommand(command)
+const empty = (s) => s
+const red = (s) => chalk.red(s)
+const green = (s) => chalk.green(s)
+
+let pastMessage = ''
+let autoCompletingNow = ''
+let pastMessageColorizer = empty
+
+process.stdin.on('keypress', async function (data, key = { name: 'unknown'}) {
+	const input = data.toString()
+	
+	if(key.name === 'c' && key.ctrl){ //enter
+		out(chalk.pink('Cancelling...'))
+		resetPrompt()
+		
+	}else if(key.name === 'return'){ //enter
+		if(!shellFocused) return
+		if(pastMessage !== ''){
+			const command = parseComand(pastMessage.split(' '))
+			await executeCommand(command)
+		}
+		resetPrompt()
+	}else if(key.name === 'backspace'){ //delete
+		if(!shellFocused) return
+		pastMessage = pastMessage.slice(0, -1)
+		process.stdout.clearLine()
+		await cleanLineAndReRender()
+		handleCorretions(data)
+		const cursor = require('term-cursor')
+		cursor.right(1)
+	
+	}else if(key.name === 'tab'){ //tab
+		pastMessage = autoCompletingNow
+		
+		process.stdout.clearLine(-1);  // clear current text
+	
+		const cursor = require('term-cursor')
+		cursor.up(1)
+		
+		const str = await getPrompt()
+		promptPrint(`${str}${pastMessage}`);
+	}else{
+		pastMessage = pastMessage.concat(input)
+		
+		handleCorretions(data)
+	}
+});
+
+async function resetPrompt(){
+	pastMessage = ''
 	shellFocused = true
-	showPrompt()
-})
+	const str = await getPrompt()
+	promptPrint(`${str}`)
+}
+
+async function handleCorretions(data){
+	if(!data) return
+	
+	const goal = getBestAutocompletion(pastMessage)
+
+	if(goal){
+		promptPrint(green(data))
+		let [_,left] = goal.split(data)
+		if(!left) return
+		promptPrint(chalk.gray(left))
+		const cursor = require('term-cursor')
+		cursor.left(left.length)
+		autoCompletingNow = goal
+	}else{
+		let tmp = pastMessage
+		pastMessage = ''
+		await cleanLineAndReRender()
+		pastMessage = tmp
+		promptPrint(chalk.red(tmp))
+		pastMessageColorizer = red
+	}
+}
+
+function cleanLineAndReRender(){
+	return new Promise( async (res) => {
+		process.stdout.clearLine(-1);  // clear current text
+
+		const cursor = require('term-cursor')
+		cursor.up(1)
+
+		const str = await getPrompt()
+		promptPrint(`${str}${pastMessageColorizer(pastMessage)}`);
+		res()
+	})
+}
+
+process.stdin.setRawMode(true);
+process.stdin.setEncoding('utf-8');
+process.stdin.resume();
+keypress(process.stdin)
 
 process.on('SIGINT', function() {
 	out(chalk.blue('\n bye ! '))
 	process.exit(0)
 })
 
-const showPrompt = async () => {
-	out(await theme.prompt(pwd))
+const getPrompt = () => {
+	return new Promise(async (res) => {
+		let prompt = await theme.prompt(pwd)
+		res(prompt)
+	})
 }
 
-showPrompt()
+getPrompt().then(a => out(a))
+
+const getBestAutocompletion = (text) => {
+	const command = parseComand(text.split(' '))
+	return autocompletions.find(t => {
+		if(t.startsWith(command.cmd)) return t
+	})
+}
+
+let autocompletions = [
+	'ls',
+	'pwd',
+	'cd',
+	'exit',
+	'clear',
+	'echo'
+]
+
+
+if(process.platform === 'win32'){
+	var paths = process.env.Path.split(';').map(p => {
+		fs.readdir(p).then(list => {
+			list.map(b => {
+				autocompletions.push(b)
+			})
+		})
+		return path.basename(p)
+	})
+}else{
+	var paths = process.env.PATH.split(';').map(p => {
+		return path.basename(p)
+	})
+	autocompletions = autocompletions.concat(paths)
+}
+
+
--- readme.md
+++ readme.md
@@ -2,6 +2,12 @@
 
 This is a 'shell' made in NodeJS.
 
+Features:
+* ls, pwd, exit, cd, echo (these are for Windows, in unix-based OS you can use the OS's built-in)
+* autocompleting for built-in commands and binaries located inside $PATH
+
+![Sample GIF](sample/sample.gif)
+
 Install dependencies:
 `npm install`
 
